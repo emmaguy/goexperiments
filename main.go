29cacc5e5b0377ce82895ac5cdb08a930e155811
Emma Guy <emmag87@gmail.com>
2016-09-02 17:13:06 +0100

    Gopher keyboard

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/atotto/clipboard"
+)
 
 func main() {
-	fmt.Println(`budumbum!! \ʕ◔ϖ◔ʔ/`)
+	var gophers = []string{`ʕ◔ϖ◔ʔ`, `\ʕ◔ϖ◔ʔ/`, `ʕﾉ◔ϖ◔ʔノ ︵ ┻━┻`}
+
+	fmt.Println("Please select which cutie you'd like on your clipboard: ")
+	for i, gopher := range gophers {
+		fmt.Printf("%d. %s\n", (i + 1), gopher)
+	}
+
+	var gopherIndex int
+	fmt.Scan(&gopherIndex)
+
+	clipboardOutput := gophers[gopherIndex-1]
+	clipboard.WriteAll(clipboardOutput)
+	fmt.Printf("Wrote \"%s\" to clipboard\n", clipboardOutput)
 }
